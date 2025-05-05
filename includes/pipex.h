bdcda8f6fb19918be68842f6c6ac206d21c71d06
Ebuzeyd Gerin <egerin@bess-f2r2s6.clusters.42paris.fr>
2025-05-05 14:15:18 +0200

    jsijdsk;g

--- includes/pipex.h
+++ includes/pipex.h
@@ -21,22 +21,24 @@
 
 typedef struct s_struct
 {
-	pid_t	pid;
-	int		pipefd[2];
-	int		f1;
-	int		f2;
-}			t_struct;
+	pid_t		pid;
+	int			pipefd[2];
+	int			f1;
+	int			f2;
+}				t_struct;
 
 typedef struct s_path
 {
-	char	*cmd_final;
-	char	*pathjoin;
-	char	**path;
-	char	**tab_cmd;
-}			t_path;
+	char		*cmd_final;
+	char		*pathjoin;
+	char		**path;
+	char		**tab_cmd;
+	t_struct	*pipex;
+}				t_path;
 
-void		free_tab(char **tab);
-char		*ft_find_path(char *cmd, char **envp);
-void		error_exit(char *str, int n);
+void			free_tab(char **tab);
+char			*ft_find_path(char *cmd, char **envp);
+void			error_exit(char *str, int n, t_struct *pipex);
+void			close_fd(t_struct *pipex);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,55 +6,66 @@
 /*   By: egerin <egerin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 15:25:00 by egerin            #+#    #+#             */
-/*   Updated: 2025/05/04 16:38:24 by egerin           ###   ########.fr       */
+/*   Updated: 2025/05/05 13:45:27 by egerin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	error_exit(char *str, int n)
+void	error_exit(char *str, int n, t_struct *pipex)
 {
+	if (pipex)
+		close_fd(pipex);
 	write(2, str, ft_strlen(str));
 	exit(n);
 }
 
-void	execute(char *cmd, char **envp)
+void	close_fd(t_struct *pipex)
+{
+	if (pipex->f1 >= 0)
+		close(pipex->f1);
+	if (pipex->f2 >= 0)
+		close(pipex->f2);
+	if (pipex->pipefd[0] >= 0)
+		close(pipex->pipefd[0]);
+	if (pipex->pipefd[1] >= 0)
+		close(pipex->pipefd[1]);
+}
+
+void	execute(char *cmd, char **envp, t_struct *pipex)
 {
 	char	**tab_cmd;
 	char	*path;
 
 	if (!cmd || !*cmd || cmd[0] == ' ')
-		error_exit("error command not found\n", 127);
+		error_exit("error command not found\n", 127, pipex);
 	tab_cmd = ft_split(cmd, ' ');
 	path = ft_find_path(tab_cmd[0], envp);
 	if (execve(path, tab_cmd, envp) == -1)
 	{
 		free_tab(tab_cmd);
-		write(2, "error execve\n", 13);
-		exit(127);
+		error_exit("error execve failed\n", 127, pipex);
 	}
 }
 
 void	routine_child(char **av, char **envp, t_struct pipex)
 {
-	dup2(pipex.f1, STDIN_FILENO);
-	dup2(pipex.pipefd[1], STDOUT_FILENO);
-	close(pipex.f1);
 	close(pipex.f2);
 	close(pipex.pipefd[0]);
-	close(pipex.pipefd[1]);
-	execute(av[2], envp);
+	dup2(pipex.f1, STDIN_FILENO);
+	dup2(pipex.pipefd[1], STDOUT_FILENO);
+	close_fd(&pipex);
+	execute(av[2], envp, &pipex);
 }
 
 void	routine_parent(char **av, char **envp, t_struct pipex)
 {
+	close(pipex.f1);
+	close(pipex.pipefd[1]);
 	dup2(pipex.f2, STDOUT_FILENO);
 	dup2(pipex.pipefd[0], STDIN_FILENO);
-	close(pipex.f1);
-	close(pipex.f2);
-	close(pipex.pipefd[0]);
-	close(pipex.pipefd[1]);
-	execute(av[3], envp);
+	close_fd(&pipex);
+	execute(av[3], envp, &pipex);
 }
 
 int	main(int ac, char **av, char **envp)
@@ -63,16 +74,16 @@ int	main(int ac, char **av, char **envp)
 	int				status;
 
 	if (ac != 5)
-		error_exit("error ./pipex infile cmd1 outfile cmd2\n", 1);
+		error_exit("error ./pipex infile cmd1 outfile cmd2\n", 1, &pipex);
 	pipex.f1 = open(av[1], O_RDONLY);
 	pipex.f2 = open(av[4], O_CREAT | O_RDWR | O_TRUNC, 0644);
 	if (pipex.f1 < 0 || pipex.f2 < 0)
-		error_exit("error file or permission denied\n", 0);
+		error_exit("error file or permission denied\n", 0, &pipex);
 	if (pipe(pipex.pipefd) == -1)
-		error_exit("error pipe\n", 1);
+		error_exit("error pipe\n", 1, &pipex);
 	pipex.pid = fork();
 	if (pipex.pid == -1)
-		error_exit("Error\n", 1);
+		error_exit("error fork\n", 1, &pipex);
 	else if (pipex.pid == 0)
 		routine_child(av, envp, pipex);
 	else
@@ -80,8 +91,5 @@ int	main(int ac, char **av, char **envp)
 		waitpid(pipex.pid, &status, 0);
 		routine_parent(av, envp, pipex);
 	}
-	close(pipex.f1);
-	close(pipex.f2);
-	close(pipex.pipefd[0]);
-	close(pipex.pipefd[1]);
+	close_fd(&pipex);
 }
